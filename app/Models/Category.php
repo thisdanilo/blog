67d4a51d0c3254aada7dadc97d3fb0d7989428a8
@danilo.cardoso <danilo.cardoso@vercan.com.br>
2022-06-24 08:55:15 -0300

    adicionado entidades category, post, tag e migrations

---
+++ app/Models/Category.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory,
+        SoftDeletes;
+
+    /**
+     * Tabela do banco de dados
+     *
+     * @var string $table
+     */
+    protected $table = 'categories';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $dates
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    /*
+	|--------------------------------------------------------------------------
+	| Relationship
+	|--------------------------------------------------------------------------
+	|
+	| Definição dos métodos das entidades relacionadas.
+	| Estes métodos são responsáveis pelas relações e permitem acessar
+	| os atributos Eloquent obtidas das mesmas.
+	|
+	*/
+
+    /**
+     * Obtêm as postagens
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory,
+        SoftDeletes;
+
+    /**
+     * Tabela do banco de dados
+     *
+     * @var string $table
+     */
+    protected $table = 'posts';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'category_id',
+        'title',
+        'slug',
+        'description',
+        'is_published'
+    ];
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_published' => 'boolean'
+    ];
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $dates
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    /*
+	|--------------------------------------------------------------------------
+	| Relationship
+	|--------------------------------------------------------------------------
+	|
+	| Definição dos métodos das entidades relacionadas.
+	| Estes métodos são responsáveis pelas relações e permitem acessar
+	| os atributos Eloquent obtidas das mesmas.
+	|
+	*/
+
+    /**
+     * Obtém a tag
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class)->withTrashed();
+    }
+
+    /**
+     * Obtêm as tags
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class)->withTrashed();
+    }
+}
